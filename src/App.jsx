b92435cda36d1641eb391b88e5b344e3b231eddf
Sarvesh <>
2026-01-14 17:02:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ClassBasedComponent from "./Topics/01_TypesOfComponent/ClassBasedComponent";
+import FunctionBasedComponent from "./Topics/01_TypesOfComponent/FunctionBasedComponent"
+import StatesInFunctionBased from "./Topics/02_States/StatesInFunctionBased";
+
+function App() {
+  return (
+    <div>
+      {/* <FunctionBasedComponent /> */}
+      {/* <ClassBasedComponent /> */}
+      <StatesInFunctionBased/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+//! REACT WAY
+import { createRoot } from "react-dom/client";
+import App from "./App";
+createRoot(document.getElementById("root")).render(<App></App>);
+
+// //! JS WAY
+// const div = document.getElementById("root");
+// const h1 = document.createElement("h1");
+// h1.textContent = "Hello React";
+// div.append(h1);
